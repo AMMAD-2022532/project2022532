b5067417700216d6ea12e1760e8fa0f8062218c5
AMMAD-2022532 <146199375+AMMAD-2022532@users.noreply.github.com>
2023-10-01 23:03:32 +0100

    Update Project2022532.java
    my name

--- src/project2022532/Project2022532.java
+++ src/project2022532/Project2022532.java
@@ -15,6 +15,7 @@ public class Project2022532 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("This is Ammad");
     }
     
 }
