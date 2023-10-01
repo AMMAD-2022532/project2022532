8a2b6ddc0f27b5551a8f35770fa59521e6256339
AMMAD-2022532 <146199375+AMMAD-2022532@users.noreply.github.com>
2023-10-01 23:01:40 +0100

    Initial commit

---
+++ src/project2022532/Project2022532.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project2022532;
+
+/**
+ *
+ * @author me
+ */
+public class Project2022532 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
